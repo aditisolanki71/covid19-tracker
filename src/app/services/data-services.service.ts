efe4b725fd72d65e168d59a9f3814c1c8a5ba39e
Unknown <adtsolanki22@gmail.com>
2020-06-20 15:12:23 +0530

    Parsing Result

--- src/app/services/data-services.service.ts
+++ src/app/services/data-services.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators'
+import { GlobalDataSummary } from '../models/global-data';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +12,32 @@ export class DataServicesService {
 
   getGlobalData() {
     return this.http.get(this.globalDataUrl, { responseType: 'text'}).pipe(map(result => {
-      return result;
+     let data: GlobalDataSummary[] = []
+      let raw = {}
+      let rows = result.split('\n')
+     rows.splice(0,1);
+     rows.forEach((row) => {
+     let cols = row.split(/,(?=\S)/)
+     let cs = {
+       country: cols[3],
+       confirmed: +cols[7],
+       deaths: +cols[8],
+       recovered: +cols[9],
+       active: +cols[10],
+     };
+     let temp : GlobalDataSummary= raw[cs.country]
+     if(temp) {
+       temp.active += cs.active
+       temp.confirmed += cs.confirmed
+       temp.recovered += cs.recovered
+       temp.deaths += cs.deaths
+       raw[cs.country] = temp;
+     }
+     else {
+       raw[cs.country] = cs;
+     }
+     })
+      return Object.values(raw);
     })
     )
   }
